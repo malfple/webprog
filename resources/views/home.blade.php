e2f44a6c3dc77ba218133388587a45a85df1ff6e
Naufal Rayfi Hafizh <naufalrayfi@gmail.com>
2018-12-02 13:49:01 +0700

    add css and add header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="navbar-left">
+                <ul>
+                    <a class="logo">Pintester</a>
+                    <input class="input" type="text"   placeholder="Search here">
+                </ul>
+            </div>
+            <div class="navbar-right">
+                <ul>
+                    <button formaction="loginForm.html">Login</button>
+                    <button formaction="registerForm">Register</button>
+                </ul>
+            </div>
+        </div>
+        
+    </body>
+</html>
