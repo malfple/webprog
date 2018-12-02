ef0ce33d8d2abb252df781ceffd4f0098a19adff
Naufal Rayfi Hafizh <naufalrayfi@gmail.com>
2018-12-02 14:32:43 +0700

    Update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,24 @@
                 </ul>
             </div>
         </div>
-        
+        <div class="content">
+            <div class="imgrow">
+                <div class="imgcol">
+                    <center>
+                        <img src="img/kanna1.jpg" alt="kanna" style="50%">
+                    </center>
+                </div>
+                <div class="imgcol">
+                    <center>
+                        <img src="img/victorique1.jpg" alt="victorique" style="50%">
+                    </center>
+                </div>
+                <div class="imgcol">
+                    <center>
+                        <img src="img/Illya.jpg" alt="Illya" style="50%">
+                    </center>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
