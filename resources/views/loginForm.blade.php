b3ea290e7a2d696b6e036e7a7188d8a15630c249
Naufal Rayfi Hafizh <naufalrayfi@gmail.com>
2018-12-02 12:09:03 +0700

    add blade.php

---
+++ resources/views/loginForm.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<link rel = "stylesheet" type="text/css" href="style.css">
+</head>
+<body class="login">
+<div class="login-page">
+  <div class="form">
+      <h3>LOGIN</h3>
+    <form class="login-form">
+      <input type="text" placeholder="email"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="registerForm.html">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</body>
+</html>
