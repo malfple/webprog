2918a00c539f98336b118683e6d777af0aa9d8cf
unknown <evanrahardja010398@gmail.com>
2018-12-02 10:40:24 +0800

    database

---
+++ app/Cart.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    //
+    public function posts(){
+    	return $this->hasMany('App\Post');
+    }
+
+    public function user(){
+    	return $this->hasOne('App\User');
+    }
+
+    public function transaction(){
+    	return $this->belongsTo('App\Transaction');
+    }
+}
--- app/Category.php
+++ app/Category.php
@@ -4,14 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Category extends Model
 {
     //
     public function posts(){
     	return $this->hasMany('App\Post');
-    }
-
-    public function cart(){
-    	return $this->belongsTo('App\Cart');
     }
 }
