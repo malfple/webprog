92698e98ef3f8b59221ea44b43cb734bf0644d18
Naufal Rayfi Hafizh <naufalrayfi@gmail.com>
2018-12-02 21:05:31 +0700

    add Followed categories

---
+++ resources/views/followedCategory.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('title', 'Followed Categories')
+
+@section('content')
+     <div class="imgrow">
+         <div class="imgcol">
+             <center>
+                 <img src="img/kanna1.jpg" alt="kanna" style="50%">
+             </center>
+         </div>
+         <div class="imgcol">
+             <center>
+                 <img src="img/victorique1.jpg" alt="victorique" style="50%">
+             </center>
+         </div>
+         <div class="imgcol">
+             <center>
+                 <img src="img/Illya.jpg" alt="Illya" style="50%">
+             </center>
+         </div>
+</div>
+@endsection
