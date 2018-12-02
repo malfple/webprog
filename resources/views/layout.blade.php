bb7e083983f3352b9987645377d04606b92caff8
Naufal Rayfi Hafizh <naufalrayfi@gmail.com>
2018-12-02 16:16:45 +0700

    Add layout.blade.php

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="navbar-left">
+                <ul>
+                    <a class="logo">Pintester</a>
+                    <input class="input" type="text"   placeholder="Search here">
+                </ul>
+            </div>
+            <div class="navbar-right">
+                <ul>
+                    <a href="home.html">Home</a>
+                    <a href="#">My Post</a>
+                    <a href="#">Post Detail</a>
+                    <a href="loginForm.html">Login</a>
+                    <a href="registerForm.html">Register</a>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            @yield('content')
+        </div>
+    </body>
+</html>
