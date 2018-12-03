213efa521e76ee05ea016c6349572a1fe6de9d55
Naufal Rayfi Hafizh <naufalrayfi@gmail.com>
2018-12-03 14:44:00 +0700

    update css and followd categories

--- resources/views/followedCategories.blade.php
+++ resources/views/followedCategories.blade.php
@@ -3,18 +3,22 @@
 @section('title', 'Followed Categories')
 
 @section('content')
-    <table>
-        <tr>
-            <td>
-                <p>Anime</p>
-                <a class="btnfollow" href="#">Follow</a>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <p>Ahegao</p>
-                <a class="btnUnfollow" href="#">Unfollow</a>
-            </td>
-        </tr>
-    </table>
+
+    <div class="categories">
+        <table>
+            <tr>
+                <td>
+                    <h2>Categories</h2>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <form>
+                        <input type="checkbox" value="ahegao"> Ahegao<br>
+                        <input type="checkbox" value="anime"> Anime<br>
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </div>
 @endsection
